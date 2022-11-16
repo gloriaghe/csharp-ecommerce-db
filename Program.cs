7a77be2260f4735bef64bf591920351bc27dadd3
gloriaghe <gloghe@yahoo.it>
2022-11-16 16:47:37 +0100

    ordine cliente

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using System.Linq.Expressions;
 EcommerceDbContext db = new EcommerceDbContext();
 bool choiceOK = false;
@@ -43,7 +44,6 @@ List<Product> products = db.Products.ToList<Product>();
 
 
 
-db.SaveChanges();
 
 while (!choiceOK)
 {
@@ -60,15 +60,47 @@ while (!choiceOK)
             Console.Write("Il tuo cognome: ");
             string surname = Console.ReadLine();
             Customer customerRicerca = db.Customers.Where(customer => customer.Name == name || customer.Surname == surname).FirstOrDefault();
+            //Employe employe1 = db.Employee.Where(employe => employe.Name == "Simonetta" ).FirstOrDefault();
+
             if (customerRicerca != null)
             {
-
                 Console.Write("Scegli la Box di prodotti da acquistare:");
                 Console.Write("1 Box Donna");
                 Console.Write("2 Box Uomo");
                 Console.Write("3 Box Luxury");
                 int choiceBox = Convert.ToInt32(Console.ReadLine());
+                bool OrderOK = false;
+                while (!OrderOK)
+                {
 
+                    Order ordine = new Order();
+                    ordine.Date = DateTime.Now;
+                    ordine.Amount = 40.50;
+                    Random random = new Random();
+                    int rnd = random.Next(0, 1);
+                    if (rnd == 1)
+                        ordine.Status = true;
+                    else
+                        ordine.Status = false;
+
+                    customerRicerca.Orders = new List<Order>();
+                    customerRicerca.Orders.Add(ordine);
+                    ordine.EmployeId = 1;
+                    db.SaveChanges();
+
+                    if (rnd == 0)
+                    {
+                        Console.WriteLine("L'ordine non è andato a buon fine. Riprovo!");
+                        OrderOK = false;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ordine effettuato!");
+                        OrderOK = true;
+                    }
+
+                }
             }
             else
             {
@@ -85,3 +117,4 @@ while (!choiceOK)
             break;
     }
 }
+db.SaveChanges();
